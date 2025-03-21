c48d102d247b7ba8ec5ed2a2da7905a2866c9de0
JoaoVtC <joao.varaujo@sptech.school>
2025-03-21 11:03:00 -0300

    Inserção do código html e js

---
+++ index.js
@@ -0,0 +1,12 @@
+function converter() {
+    var decimal = Number(demo1.value);
+
+    demo2.innerHTML = decimal.toString(8);
+    demo3.innerHTML = decimal.toString(16);
+    demo4.innerHTML = decimal.toString(2);
+} // operações em bases diferentes
+    // subtração
+    // divisão
+    // adição
+    // multiplicação
+    // usuário poder inserir diferentes bases númericas
